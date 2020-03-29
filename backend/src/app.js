e2d59aef76ef989132c3fd191c9e2eaaa8fb1914
Renan Bottero <renanbottero@gmail.com>
2020-03-29 11:37:58 -0300

    Validações e testes unitários do back-end

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 
 
 const app = express();
 
+// Production
+/*app.use(cors({
+    origin: 'http://meuapp.com'
+}));
+*
+*/
+
 app.use(cors());
+
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 
-
-
-
-app.listen(3333);
+module.exports = app;
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,12 +1,12 @@
 const connection = require('../database/connection');
+const generateUniqueId = require('../utils/generateUniqueId');
 
-const crypto = require('crypto');
 
 module.exports = {
     async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
     
         await connection('ongs').insert({
             id,
